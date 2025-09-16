c3d23f2902a0b4fe71844c3dcf805d2df1584c35
amrutaybhandari <amrutaybhandari@gmail.com>
2025-09-16 07:32:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TeamCSK from './IPL_Component/TeamCSK';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TeamCSK/>
     </div>
-  );
+    );
 }
-
 export default App;
---
+++ src/IPL_Component/TeamCSK.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'C:/Users/hp/Desktop/Amruta Bhandari/Ps_211_ReactJs/demo-app/src/IPL_Component_CSS/TeamCSK.css'
+function TeamCSK()
+{
+    return(
+        <div id="csk">
+         <h1 style={{color:'Red'}}>Team Name:</h1>   <h1>CSK</h1>
+         <h1 style={{color:'Red'}}>Captain Name:</h1>   <h1>Ruturaj G.</h1>
+        </div>
+    );
+}
+export default TeamCSK;
